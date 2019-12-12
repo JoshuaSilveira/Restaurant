a3d45bf1d8dbe0c06a5ffe33f58eee99c90af700
Cynthia Lipinski <l.cynthia@live.com>
2019-12-12 11:08:20 -0500

    specials

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,22 @@
      elmnt.style.backgroundColor = color;
 
  }
+
+ //specials
+ var coll = document.getElementsByClassName("collapsible");
+ var q;
+
+ for (q = 0; q < coll.length; q++) {
+     coll[q].addEventListener("click", function () {
+         this.classList.toggle("active");
+         var content = this.nextElementSibling;
+         if (content.style.display === "block") {
+             content.style.display = "none";
+         } else {
+             content.style.display = "block";
+         }
+     });
+ }
  // Get the element with id="defaultOpen" and click on it
  document.getElementById("defaultOpen").click();
 

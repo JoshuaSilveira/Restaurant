95c25eab2e9b3111e4da4347f08d31232e8b1c32
Cynthia Lipinski <l.cynthia@live.com>
2019-12-12 19:27:04 -0500

    footer

--- scripts/script.js
+++ scripts/script.js
@@ -37,38 +37,3 @@ for (q = 0; q < coll.length; q++) {
         }
     });
 }
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
-}
